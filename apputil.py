18d87a2ccd4a9be6cd0fc64e5019b9de5500144f
varunrapolu1612 <rvarun@iu.edu>
2025-09-16 21:36:02 -0400

    Update apputil.py

--- apputil.py
+++ apputil.py
@@ -1,5 +1,33 @@
 import seaborn as sns
 import pandas as pd
 
+#EXERCOSE: 1
 
-# update/add code below ...
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+#EXERCOSE: 2
+
+def to_binary(n):
+    # Base case
+    if n == 0:
+        return "0"
+    elif n == 1:
+        return "1"
+    else:
+        return to_binary(n // 2) + str(n % 2)
+
+#EXERCOSE: 3
+
+
+
+
+
+
+
+
+
+
